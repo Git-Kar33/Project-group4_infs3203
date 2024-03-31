2be2cf61024bc46afb9c95cf9013c730883ab366
Git-Kar33 <141827107+Git-Kar33@users.noreply.github.com>
2024-03-31 11:28:19 +0300

    Update persistence.js

--- persistence.js
+++ persistence.js
@@ -1,49 +1,103 @@
-// MongoDB connection module
-const mongodb = require('mongodb');
+const mongodb = require('mongodb')
 
-// MongoDB client and database initialization
-let client = undefined;
-let db = undefined;
-let user = undefined;
-let session = undefined;
-let customerData = undefined;
+let client = undefined
+let db = undefined
+let user = undefined
+let session = undefined
+let customerData = undefined
+let pointHistory = undefined
 
-// Function to connect to MongoDB database
+
 async function connectDatabase() {
-    if (!client || !client.isConnected()) {
-        client = new mongodb.MongoClient("mongodb+srv://60084212:12class34@cluster0.homew0b.mongodb.net/");
-        db = client.db('INFS3203');
-        user = db.collection('user');
-        customerData = db.collection('customerData');
-        session = db.collection('SessionData');
-        await client.connect();
+    if (!client) {
+        client = new mongodb.MongoClient("mongodb+srv://60102524:12class34@cluster0.u1ve744.mongodb.net/")
+        db = client.db('INFS3203')
+        user = db.collection('user')
+        customerData = db.collection('customerData')
+        session = db.collection('SessionData')
+        pointHistory = db.collection('pointsData')
+        await client.connect()
     }
 }
 
-// Function to insert user data into the database
-async function insertUser(data) {
+async function insertUser(data){
     await connectDatabase();
-    await user.insertOne(data);
+    await user.insertOne(data)
 }
 
-// Function to insert customer data into the database
-async function insertCustomer(data) {
+async function insertCustomer(data){
     await connectDatabase();
-    await customerData.insertOne(data);
+    await customerData.insertOne(data)
 }
 
-// Function to get user details from the database based on username
+async function insertPointData(data){
+    await connectDatabase();
+    await pointHistory.insertOne(data)
+}
+
 async function getUserDetails(userName) {
     await connectDatabase();
     let users = await user.find({username:userName});
     let userDetails = await users.toArray();
     return userDetails[0];
-
-// Function to get customer details from the database based on qid
-async function getCustomerDetails(qid) {
- await connectDatabase();
-    let customer = await customerData.find({qid:qid});
-    let customerDetails = await customer.toArray();
-    return customerDetails[0]; 
 }
 
+async function getPointHistory(qid) {
+    await connectDatabase();
+    let userPointHistory = await pointHistory.find({qid:qid}).sort({date: -1});
+    let userPointHistoryData = await userPointHistory.toArray();
+    return userPointHistoryData;
+}
+
+async function getCustomerDetails(qid) {
+    await connectDatabase();
+    let customer = await customerData.find({qid:qid});
+    let customerDetails = await customer.toArray();
+    return customerDetails[0];
+}
+
+async function addRecord(date, qid, wasteType, category, weight, pointsRecieved){
+    await connectDatabase();
+    let customerDetails = await getCustomerDetails(qid);
+    customerDetails.totalPoints += pointsRecieved
+    for (let c of customerDetails.dateRecord){
+        if (c.date==date){
+            c.data.push({wasteType: wasteType, category:category, weight:weight, points: pointsRecieved})
+            await customerData.updateOne({qid: qid, "dateRecord.date": date}, {$set:{"dateRecord.$.data": c.data, totalPoints: customerDetails.totalPoints}})
+            return
+        }
+    }
+    await customerData.updateOne({qid: qid}, {$push: {"dateRecord": {date: date, data: [{wasteType: wasteType, category:category, weight:weight, points: pointsRecieved}]}}, $set:{totalPoints: customerDetails.totalPoints}})
+}
+
+async function updatePoints(qid, points){
+    await connectDatabase();
+    await customerData.updateOne({qid: qid}, {$set: {totalPoints: points}})
+}
+
+async function saveSession(uuid, expiry, data) {
+    await connectDatabase();
+    let sessionData = {
+        sessionKey:uuid,
+        expiry: expiry,
+        data:data
+    }
+    await session.insertOne(sessionData)
+}
+
+async function getSessionData(key) {
+    await connectDatabase();
+    let sessiond= await session.find({sessionKey:key}) 
+    let sessionData = await sessiond.toArray();
+
+    return sessionData[0];
+}
+
+async function deleteSession(key){
+    await connectDatabase();
+    await session.deleteOne({sessionKey:key});
+}
+
+module.exports={
+    insertUser, insertCustomer, insertPointData, getUserDetails, getPointHistory, addRecord, getCustomerDetails, updatePoints, saveSession, getSessionData, deleteSession
+}
