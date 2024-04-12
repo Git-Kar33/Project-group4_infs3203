aaedd8b8db3a53732f97521a7dfd6e3cbd14303b
Git-Kar33 <141827107+Git-Kar33@users.noreply.github.com>
2024-04-12 06:02:10 +0300

    Update server.test.js

--- server.test.js
+++ server.test.js
@@ -81,3 +81,5 @@ const persistence = require("./persistence.js")
         expect(pointData.point).toBe(pointHistory[0].point);
         await business.deletePointData(pointData.qid);
     });
+//tacer
+//tacer
